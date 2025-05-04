5458daf2c7fe4933496a7e3a38726b282129889b
Affan Suhendar <affans@mhs.usk.ac.id>
2025-05-04 19:56:48 +0700

    Update

--- src/components/cards/departmentCard/DepartmentList.tsx
+++ src/components/cards/departmentCard/DepartmentList.tsx
@@ -3,12 +3,12 @@ import { departments } from "@/components/data/departments";
 
 const DepartmentList = () => {
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full h-full items-center justify-center ">
       {departments.map((dept) => (
         <DepartmentCard key={dept.id} id={dept.id} name={dept.name} description={dept.desc} />
       ))}
     </div>
-  );
+  );  
 };
 
 export default DepartmentList;
--- src/components/sections/departmentList/DepartmentList.tsx
+++ src/components/sections/departmentList/DepartmentList.tsx
@@ -2,40 +2,61 @@ import Image from "next/image";
 import logoAtas from "@/components/svg/departmentAsset/logoAtas.svg";
 import logoBawah from "@/components/svg/departmentAsset/logoBawah.svg";
 import logoKananBawah from "@/components/svg/departmentAsset/logoKananBawah.svg";
-
+import BlurBawah from "@/components/svg/departmentAsset/Blur.svg";
+import BlurAtas from "@/components/svg/departmentAsset/Blur.svg";
 import DepartmentList from "@/components/cards/departmentCard/DepartmentList";
+import Background from "@/components/svg/departmentAsset/BackgroundGaris.svg";
 
 const Department_List = () => {
   return (
-    <main className="w-screen h-screen relative bg-[#0D0C1D] flex justify-center items-center">
+    <main className="w-screen h-screen relative bg-[#0D0C1D] flex justify-center items-center overflow-hidden">
 
       {/* Flayer */}
       
-
       {/* Background Decorations */}
       <div className="gambar">
+
+        <Image 
+          src={Background} 
+          alt="Background" 
+          className="absolute w-screen top-0 left-0 opacity-70"
+        />
+
+        <Image 
+          src={BlurAtas} 
+          alt="Blur Atas" 
+          className="absolute top-[-150px] left-0 w-full opacity-50"
+        />
+
         <Image 
           src={logoAtas} 
           alt="Logo Atas" 
-          className="absolute w-60 top-0 left-0" 
+          className="absolute w-50 top-0 left-0" 
         />
         <Image 
           src={logoBawah} 
           alt="Logo Bawah" 
-          className="absolute w-80 bottom-0 left-0" 
+          className="absolute w-60 bottom-[-80px] left-0" 
         />
         <Image 
           src={logoKananBawah} 
           alt="Logo Kanan Bawah" 
-          className="absolute w-80 bottom-0 right-0" 
+          className="absolute w-60 bottom-0 right-0" 
         />
+
+        <Image 
+          src={BlurBawah} 
+          alt="Blur Bawah" 
+          className="absolute bottom-[-150px] left-0 w-full " 
+        />
+
       </div>
 
       {/* Main Content */}
-      <div className="w-[80%] z-1">
+      <div className="w-[80%] h-[70%] z-1">
         <DepartmentList />
-      </div>
-    </main>
+      </div>  
+    </main> 
   );
 };
 
