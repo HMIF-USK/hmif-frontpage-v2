b0b10b61ebd7ddf2d97532124596dc28dc07b1a7
Affan Suhendar <affans@mhs.usk.ac.id>
2025-05-04 17:07:45 +0700

    Update

--- src/components/data/departments.ts
+++ src/components/data/departments.ts
@@ -1,8 +1,8 @@
 export const departments = [
-    { id: 1, name: "KOMINKRAF", desc: "Komunikasi, informasi dan perencanaan kreativitas" },
+    { id: 1, name: "KOMINKRAF", desc: "Komunikasi, Informasi dan Perencanaan Kreativitas" },
     { id: 2, name: "PPM", desc: "Penelitian dan Pengembangan Mahasiswa" },
     { id: 3, name: "HUAL", desc: "Hubungan Antar Lembaga" },
-    { id: 4, name: "PKM", desc: "Pengelolaan Kesejahteraan mahasiswa" },
+    { id: 4, name: "PKM", desc: "Pengelolaan Kesejahteraan Mahasiswa" },
     { id: 5, name: "SOSMAS", desc: "Sosial Masyarakat" },
   ];
 
